21a1261dfe1815fde4c0f9bab1f09bf7e9e1df62
ElsaW000 <lorindaw000@gmail.com>
2025-05-14 15:19:22 +0100

    Update properties.js

--- src/properties.js
+++ src/properties.js
@@ -41,7 +41,12 @@ var tagCloudOptions = [{
     field: 'series',
     title: 'Series',
     minTagFrequency: 1
-}];
+},{
+    field: 'type',
+    title: 'Type',
+    minTagFrequency: 1
+  }
+];
 
 /**
  * If BibTeX entries (and tags) should be editable
